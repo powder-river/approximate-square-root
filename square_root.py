4da8b208d9636f3f9e500b1c192c8242820491fc
Danai Adkisson <dadkisso@live.com>
2015-09-02 00:09:23 -0400

    delete commented code

--- square_root.py
+++ square_root.py
@@ -1,12 +1,4 @@
-
-
 user_input = float(input("Please enter a positive number...\n"))
-#ballpark_num = float(input("Please enter your ballpark_num\n"))
-
-
-
-    #perform Newton's method of successive approximations to
-#approximate the square root of the inputted number
 
 def square_root(x):
      count = 0
@@ -17,14 +9,4 @@ def square_root(x):
          count += 1
          print("Count is {} number is {}".format(count,ballpark_num))
      return new_number
-
-
-
 print("The Square Root of {} is {}".format(user_input, square_root(user_input)))
-
-#hard
-# Each time through the loop,
-# print out the number of iterations that have occurred
-
-# Show an error message if the user enters any invalid
-# input (negative number or non-numeric string)
